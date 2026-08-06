6e38e50709e75427e2c3ff51ce6bee20a70a006b
coxhorse143-beep <coxhorse143@gmail.com>
2026-08-06 11:12:18 +0700

    Update login.js

--- login.js
+++ login.js
@@ -56,3 +56,45 @@ loginForm.addEventListener("submit", function(e){
     }
 
 });
+function login(){
+
+
+let username =
+document.getElementById("username").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+if(
+username=="admin" &&
+password=="admin123"
+){
+
+
+localStorage.setItem(
+"login",
+"true"
+);
+
+
+
+window.location.href=
+"dashboard.html";
+
+
+
+}else{
+
+
+alert(
+"Username atau password salah"
+);
+
+
+}
+
+
+}
