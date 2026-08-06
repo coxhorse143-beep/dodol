1da96fa5760ea904498129fb90119b4f0d0620d0
coxhorse143-beep <coxhorse143@gmail.com>
2026-08-06 10:57:09 +0700

    Create cart.js

---
+++ cart.js
@@ -0,0 +1,80 @@
+// DATA KERANJANG
+
+let keranjang =
+JSON.parse(
+localStorage.getItem("keranjang")
+) || [];
+
+
+
+
+// TAMBAH KERANJANG
+
+function tambahKeranjang(){
+
+
+let id =
+localStorage.getItem(
+"produkDetail"
+);
+
+
+
+let produkCart =
+produk.find(
+(item)=>item.id==id
+);
+
+
+
+let cek =
+keranjang.find(
+(item)=>item.id==id
+);
+
+
+
+if(cek){
+
+
+cek.jumlah += 1;
+
+
+}else{
+
+
+keranjang.push({
+
+id:produkCart.id,
+
+nama:produkCart.nama,
+
+harga:produkCart.harga,
+
+gambar:produkCart.gambar,
+
+jumlah:1
+
+});
+
+
+}
+
+
+
+localStorage.setItem(
+
+"keranjang",
+
+JSON.stringify(keranjang)
+
+);
+
+
+
+alert(
+"Produk masuk keranjang"
+);
+
+
+}
